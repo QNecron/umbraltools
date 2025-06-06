ec006835ed1f5cbdd56eafa1d6700ba86ba9e784
Jacob <q.necron@gmail.com>
2025-06-06 14:32:40 -0500

    Spells!
    Spells page up and running, filters to come later.

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -6,9 +6,9 @@ interface propsCard {
   imageHeight?: number;
   imageWidth?: number;
   title: string;
-  badge?: string;
-  copy1?: string;
-  copy2?: string;
+  badge?: any;
+  copy1?: any;
+  copy2?: any;
   copy3?: any;
   copy4?: any;
   copy5?: any;
@@ -51,7 +51,7 @@ export default function Wrapper({
       <div className="card__content">
         <h2 className="card__title" heading="3">{title}</h2>
         {badge && <div className="card__badge">{badge}</div>}
-        {copy1 && <p className="card__copy card__copy-1">{copy1}</p>}
+        {copy1 && <div className="card__copy card__copy-1">{copy1}</div>}
         {copy2 && <div className="card__copy card__copy-2">{copy2}</div>}
         {copy3 && <div className="card__copy card__copy-3">{copy3}</div>}
         {copy4 && <div className="card__copy card__copy-4">{copy4}</div>}
--- app/routes/spells.tsx
+++ app/routes/spells.tsx
@@ -9,16 +9,16 @@ import Card from "../components/card";
 import Desktop from "../images/hero_desktop_16-4-5.webp";
 import Mobile from "../images/hero_mobile_1-1.webp";
 
-import FeatsData from "../data/feats.json";
+import SpellsData from "../data/spells.json";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "Feats | Umbral Tools" },
+    { title: "Spells | Umbral Tools" },
     { name: "description", content: "Toolset for the ShadowDark TTRPG." },
   ];
 };
 
-export default function Feats() {
+export default function Spells() {
  
   return(
     <>
@@ -39,16 +39,25 @@ export default function Feats() {
     <Section>
       <Wrapper>
         <Grid>
-          {FeatsData.sort((a, b) => a.name < b.name ? -1 : 1).map((feat, index) => { 
+          {SpellsData.sort((a, b) => a.name < b.name ? -1 : 1).map((spell, index) => {
+                        
+            let spellClass = <> <strong>Class:</strong> {spell.class} </>;
+            
+            let spellDuration = <> <strong>Duration:</strong> {spell.duration} </>;
+
+            let spellRange = <> <strong>Range:</strong> {spell.range} </>;
+            
             return(
               <Card 
                 key={index} 
-                theme="feats" 
-                title={feat.name} 
-                copy1={feat.description} 
-                copy2={feat.action} 
-                copy3={feat.type} 
-                copy4={feat.stacking === true ? "Stacks : True" : "Stacks : False"}
+                theme="spells" 
+                title={spell.name} 
+                badge={spell.tier}
+                copy1={spellClass}
+                copy2={spellDuration}
+                copy3={spellRange}
+                copy4={spell.description_1} 
+                copy5={spell.description_2}
               />
             );
           })}
